f402a12675808239c2ecaffe951bff77cc2a084e
Erik Jenks <erikjenks@gmail.com>
2015-03-01 17:17:34 -0500

    transfer funds to seller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,8 +2,6 @@ class OrdersController < ApplicationController
   before_action :set_order, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
 
-  # GET /orders
-  # GET /orders.json
   
   def sales
     @orders = Order.all.where(seller: current_user).order("created_at DESC")
@@ -13,20 +11,13 @@ class OrdersController < ApplicationController
     @orders = Order.all.where(buyer: current_user).order("created_at DESC")
   end
 
-  # GET /orders/1
-  # GET /orders/1.json
-  def show
-  end
 
-  # GET /orders/new
   def new
     @order = Order.new
     @listing = Listing.find(params[:listing_id])
   end
 
 
-  # POST /orders
-  # POST /orders.json
   def create
     @order = Order.new(order_params)
     @listing = Listing.find(params[:listing_id])
@@ -48,7 +39,13 @@ class OrdersController < ApplicationController
       flash[:notice] = "Thanks for ordering!"
     rescue Stripe::CardError => e
       flash[:danger] = e.message
-    end    
+    end
+
+    transfer = Stripe::Transfer.create(
+      :amount => (@listing.price * 95).floor,
+      :currency => "usd",
+      :recipient => @seller.recipient
+      )    
 
 
     respond_to do |format|
